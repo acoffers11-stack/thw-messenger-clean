3d8d62b41d71ae8e595576f101dc02b698b5c8d5
acoffers11-stack <acoffers11@gmail.com>
2026-03-12 02:22:37 +0530

    fix railway port

--- server.js
+++ server.js
@@ -278,6 +278,8 @@ io.emit("user list",Object.keys(users))
 
 /* SERVER */
 
-server.listen(3000,()=>{
-console.log("Server running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+console.log("Server running on port " + PORT);
+});

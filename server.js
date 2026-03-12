fe5ed0e9f8c38f4c16461a9befbeb28724badf11
acoffers11-stack <acoffers11@gmail.com>
2026-03-12 08:34:22 +0530

    server update

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ async function sendVerificationEmail(email, code) {
 try {
 
 await resend.emails.send({
-from: "Chat App <team@thehimalayanwolf.com>",
+from: "team@thehimalayanwolf.com",
 to: email,
 subject: "Your verification code",
 html: `<h1>${code}</h1>`
